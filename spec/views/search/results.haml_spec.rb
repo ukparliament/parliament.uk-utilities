7ec688cee289140a45a6f9ba0d4665728095261c
Rebecca Appleyard <rklappleyard@gmail.com>
2017-07-11 09:43:34 +0100

    Completed migration of search

--- spec/views/search/results.haml_spec.rb
+++ spec/views/search/results.haml_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe 'search/results', vcr: true do
     before do
       controller.params = { q: 'banana' }
 
+      assign(:results_total, 1)
+      assign(:start_page, 1)
+      assign(:count, 1)
+      assign(:results, double(:results,
+        entries: [double(:entry,
+          title: 'Title 1',
+          url: 'Url 1',
+          summary: 'Summary 1')
+        ]))
+
       render
     end
 
     context 'headers' do
       it 'will render the correct header' do
         expect(rendered).to match(/Search/)
+      end
+    end
+
+    context 'results' do
+      it 'will render the title, url and summary for each entry' do
+        expect(rendered).to match(/Title 1/)
+        expect(rendered).to match(/Url 1/)
+        expect(rendered).to match(/Summary 1/)
       end
     end
 
@@ -23,6 +41,8 @@ RSpec.describe 'search/results', vcr: true do
 
   context 'with an invalid search term' do
     before do
+      assign(:results_total, 0)
+
       controller.params = { q: 'fsdfs' }
 
       render
@@ -35,7 +55,7 @@ RSpec.describe 'search/results', vcr: true do
     end
 
     context 'results' do
-      it 'will render the correct results message' do
+      it 'will render the no results message' do
         expect(rendered).to match(/There are no results for your search./)
       end
     end
