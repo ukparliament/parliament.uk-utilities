e5cf46861a3a73c25f5b4c5f0ea733c05b38b9df
Peter Hunt <naseberry@users.noreply.github.com>
2017-10-31 14:37:00 +0000

    [WEBDEV-393] Configured feature: pingdom

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :disable_top_navigation, :disable_status_banner
   before_action :data_check, :build_request, except: :index
 
+  before_action :enable_top_navigation, :enable_status_banner, except: :index
+  before_action :enable_pingdom, only: :mps
+
   ROUTE_MAP = {
     mps: proc { Parliament::Utils::Helpers::ParliamentHelper.parliament_request.person_mps },
     find_your_constituency: proc { Parliament::Utils::Helpers::ParliamentHelper.parliament_request.find_your_constituency }
@@ -11,9 +14,6 @@ class HomeController < ApplicationController
   def index; end
 
   def mps
-    enable_top_navigation
-    enable_status_banner
-
     @parliaments, @parties, @speaker = Parliament::Utils::Helpers::RequestHelper.filter_response_data(
         @request,
         Parliament::Utils::Helpers::RequestHelper.namespace_uri_schema_path('ParliamentPeriod'),
@@ -25,9 +25,6 @@ class HomeController < ApplicationController
   end
 
   def find_your_constituency
-    enable_top_navigation
-    enable_status_banner
-
     @regions = Parliament::Utils::Helpers::RequestHelper.filter_response_data(
       @request,
       'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'
