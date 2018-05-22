e4ccaa3cf35b8b17828c50fe493c43d8c03191a0
James Vinter <>
2018-05-22 10:44:42 +0100

    [WEBDEV-555] Homepage update with new design and gtp content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_action :disable_top_navigation, :disable_status_banner
   before_action :data_check, :build_request, except: :index
 
-  before_action :enable_top_navigation, :enable_status_banner, except: :index
+  before_action :enable_top_navigation, except: :index
+  before_action :enable_status_banner
   before_action :enable_pingdom, only: :mps
 
   ROUTE_MAP = {
